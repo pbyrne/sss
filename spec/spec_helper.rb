e8783d941bc6b56c8d2bba2e58b9e485133520e9
Patrick Byrne <patrick.byrne@tstmedia.com>
2011-10-02 18:59:47 -0500

    Build test workspace programatically, rather than be part of the repo

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,13 @@
 require 'sss'
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    set_up_test_directories
+  end
+end
+
+def set_up_test_directories
+  system "mkdir -p spec/support/workspace/project1/.git"
+  system "mkdir -p spec/support/workspace/project2/.hg"
+  system "mkdir -p spec/support/workspace/project3/.svn"
+end
